219c1a6cbb17f0193220d6faac53296d7b1e773e
Saloni Rathod <salonirathod0962@gmail.com>
2025-07-10 18:59:54 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+// main.js
+
+// Find the #app div
+const app = document.getElementById('app');
+
+// Set some content
+app.innerHTML = `
+  <h1>Welcome to Akiraka!</h1>
+  <p>This is a basic Vite project deployed on Vercel.</p>
+`;
